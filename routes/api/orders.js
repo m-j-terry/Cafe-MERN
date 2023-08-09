e8389794a924e8311a41517689c195eda5cade0c
m-j-terry <michaeljterry0815@gmail.com>
2023-08-09 19:58:25 -0400

    orders routes

---
+++ routes/api/orders.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const ordersCtrl = require('../../controllers/api/orders')
+
+/* REMEMBER: routes send through /api/orders/ as their root */
+// GET
+router.get('/cart', ordersCtrl.cart)
+// GET
+router.get('/history', ordersCtrl.history)
+// POST
+router.post('/cart/items/:id', ordersCtrl.addToCart)
+// POST
+router.post('/cart/checkout', ordersCtrl.checkout)
+// POST
+router.post('/cart/qty', ordersCtrl.setItemQtyInCart)
+
+module.exports = router
