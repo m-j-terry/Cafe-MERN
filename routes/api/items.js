ed9d3a2beebe95809dd184b6a385cfcb575f7ad5
m-j-terry <michaeljterry0815@gmail.com>
2023-08-09 20:00:46 -0400

    items routes

---
+++ routes/api/items.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const itemsCtrl = require('../../controllers/api/items')
+
+/* REMEMBER: all routes send through /api/items/ as their root */
+// GET
+router.get('/', itemsCtrl.index)
+// GET
+router.get('/:id', itemsCtrl.show)
+
+module.exports = router
