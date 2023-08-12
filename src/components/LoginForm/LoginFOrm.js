b2811dd481281085770cc863c1021585af49f190
m-j-terry <michaeljterry0815@gmail.com>
2023-08-12 06:16:35 -0400

    send request + minor changes

--- src/components/LoginForm/LoginFOrm.js
+++ src/components/LoginForm/LoginFOrm.js
@@ -1,29 +1,29 @@
-import { useState } from 'react';
-import * as usersService from '../../utilities/users-service';
+import { useState } from 'react' 
+import * as usersService from '../../utilities/users-service' 
 
 export default function LoginForm({ setUser }) {
 const [credentials, setCredentials] = useState({
     email: '',
     password: ''
-});
-const [error, setError] = useState('');
+}) 
+const [error, setError] = useState('') 
 
 function handleChange(evt) {
-    setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
-    setError('');
+    setCredentials({ ...credentials, [evt.target.name]: evt.target.value }) 
+    setError('') 
 }
 
 async function handleSubmit(evt) {
   // Prevent form from being submitted to the server
-    evt.preventDefault();
+    evt.preventDefault() 
     try {
         // The promise returned by the signUp service method
         // will resolve to the user object included in the
         // payload of the JSON Web Token (JWT)
-        const user = await usersService.login(credentials);
-        setUser(user);
+        const user = await usersService.login(credentials) 
+        setUser(user) 
     } catch {
-        setError('Log In Failed - Try Again');
+        setError('Log In Failed - Try Again') 
     }
 }
 
@@ -38,7 +38,7 @@ async function handleSubmit(evt) {
             <button type="submit">LOG IN</button>
             </form>
         </div>
-        <p className="error-message">&nbsp;{error}</p>
+        <p className="error-message">&nbsp {error}</p>
     </div>
-    );
+    ) 
 }
